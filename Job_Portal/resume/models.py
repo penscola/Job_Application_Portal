40f47058fb57497991ff53839d381bb95f2a2cc7
penscola <penscolashackletonfelix@gmail.com>
2023-11-25 22:28:12 +0300

    Logic building :technologist:

--- Job_Portal/resume/models.py
+++ Job_Portal/resume/models.py
@@ -1,14 +1,15 @@
+
 from django.db import models
 
-from Job_Portal.Job_Portal_App.models import User
+from users.models import User
 
 
 # Create your models here.
 class Resume(models.Model):
     objects = None
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    jot_title = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    surname = models.CharField(max_length=100, null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
+    jot_title = models.CharField(max_length=100, null=True, blank=True)
 #     insert cv
--- Job_Portal/users/apps.py
+++ Job_Portal/users/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class JobPortalAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Job_Portal_App'
+    name = 'users'
